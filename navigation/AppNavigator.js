29879fd6270045a1a58b3fa85d46ced7f1b121fc
joeric1028 <joeric1028@icloud.com>
2019-04-27 09:05:35 +0800

    Upgraded EXPO SDK
    From v28.0.0 to 32.0.0.
    App is working properly with minor bugs for android.

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,6 +1,6 @@
-import { createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import LoginScreen from '../screens/LoginScreen';
 
-export default createSwitchNavigator({
+export default createAppContainer(createSwitchNavigator({
   Login: LoginScreen,
-});
+}));
